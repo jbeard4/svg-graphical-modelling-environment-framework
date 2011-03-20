0fc3116128e0d5e1e8b4ccfba52215ca7f2b8f8b
jbeard4 <jbeard4@29373c21-0b15-0410-bdb5-f8d6ddd024e2>
2011-03-20 00:51:47 +0000

    Added more constraints to Class constructor. Works well.
    git-svn-id: https://msdl.cs.mcgill.ca/svn/AToM3/sandbox/jbeard4@339 29373c21-0b15-0410-bdb5-f8d6ddd024e2

--- constructors.js
+++ constructors.js
@@ -4,7 +4,7 @@
 **/
 
 
-function setupConstructors(defaultStatechartInstance,constraintModule){
+function setupConstructors(defaultStatechartInstance,cm){
 
 	//FIXME: we also need a way to delete stuff. which will mean deleting the corresponding elements in the constraint graph. need ot think about how best to do that, what that will mean. can imagine it bubbling out... like, arrows should be deleted if the thing that they target gets deleted... so maybe a more sophisticated rollback for the CS is needed?
 	return {
@@ -24,10 +24,24 @@ function setupConstructors(defaultStatechartInstance,constraintModule){
 			*/
 
 
-			var constraint = constraintModule.Constraint(
-				constraintModule.NodeAttr(nameContainerRect,"width"),
-				constraintModule.NodeAttr(nameText,"width")
-			);
+			var constraints = [
+				cm.Constraint(
+					cm.NodeAttr(nameContainerRect,"width"),
+					cm.NodeAttr(nameText,"width")
+				),
+				cm.Constraint(
+					cm.NodeAttr(nameContainerRect,"x"),
+					cm.NodeAttr(nameText,"x")
+				),
+				cm.Constraint(
+					cm.NodeAttr(nameContainerRect,"y"),
+					cm.NodeAttr(nameText,"y")
+				),
+				cm.Constraint(
+					cm.NodeAttr(nameContainerRect,"height"),
+					cm.NodeAttr(nameText,"height")
+				)
+			]
 
 			//constraintGraph.push(constraint); 
 			//visualObjectList.push(nameContainerRect,nameText);
@@ -35,7 +49,7 @@ function setupConstructors(defaultStatechartInstance,constraintModule){
 			//TODO: hook up events...
 
 			return {
-				constraints:[constraint],
+				constraints:constraints,
 				constraintVisualObjects:[nameContainerRect,nameText]
 			};
 		},
