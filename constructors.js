cd3f175c4a67c67505a57687f8816617b3f8f487
jbeard4 <jbeard4@29373c21-0b15-0410-bdb5-f8d6ddd024e2>
2011-03-24 00:31:53 +0000

    Drag-and-drop between different parents now working pretty well.
    git-svn-id: https://msdl.cs.mcgill.ca/svn/AToM3/sandbox/jbeard4@364 29373c21-0b15-0410-bdb5-f8d6ddd024e2

--- constructors.js
+++ constructors.js
@@ -6,6 +6,10 @@
 
 function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestLayout,svg,edgeLayer,nodeLayer){
 
+	function removeFromList(element,list){
+		return list.splice(list.indexOf(element),1);
+	}
+
 	//FIXME: we also need a way to delete stuff. which will mean deleting the corresponding elements in the constraint graph. need ot think about how best to do that, what that will mean. can imagine it bubbling out... like, arrows should be deleted if the thing that they target gets deleted... so maybe a more sophisticated rollback for the CS is needed?
 	return {
 		ClassIcon : function(x,y){
@@ -86,38 +90,6 @@ function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestL
 					cm.NodeAttrExpr(attributeListRect,["y","height"],cm.sum)
 				)
 			);
-
-
-			/*
-			Constraint(
-				NodeAttr(attributeListRect,"width"),
-				[NodeAttr(nameText,"width"),NodeAttr(attributeListAttr1,"width"),NodeAttr(attributeListAttr2,"width")],
-				Math.max
-			),
-			Constraint(
-				NodeAttr(attributeListRect,"height"),
-				[NodeAttr(attributeListAttr1,"height"),NodeAttr(attributeListAttr2,"height")],
-				sum
-			),
-			Constraint(
-				NodeAttr(attributeListRect,"y"),
-				NodeAttr(nameText,["y","height"],sum)
-			),
-			Constraint(
-				NodeAttr(attributeListRect,"x"),
-				NodeAttr(nameText,"x")
-			),
-
-			//attributeListAttr1
-			Constraint(
-				NodeAttr(attributeListAttr1,"y"),
-				NodeAttr(nameText,["y","height"],sum)
-			),
-			Constraint(
-				NodeAttr(attributeListAttr1,"x"),
-				NodeAttr(nameText,"x")
-			),
-			*/
 
 			var attributes = [];
 
@@ -322,59 +294,100 @@ function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestL
 				$(classContainerRect).removeClass("highlighted");
 			}
 
+			/*
 			var classContainerRectXConstraint,
 				classContainerRectYConstraint,
 				classContainerRectWidthConstraint,
 				classContainerRectHeightConstraint;
+			*/
 
-			var classContainerRectChildren = [];	//TODO: we could encode this in DOM
+			//make these things public properties... make them addressable as "dropconstraints"
+
+			icon.classContainerRectChildren = [];	//TODO: we could encode this in DOM
 
 			classContainerRect.dropShape = function(shape){
+				//debugger;
 				console.log("here");
 
 				//modify the shape's ctm to match the parent's
 				//TODO: move this out into SVG helper lib?
-				var m2 = classContainerRect.getCTM().inverse();
+				var m2 = this.getCTM().inverse();
 
 				var tl = shape.transform.baseVal;
-				var t = tl.numberOfItems ? tl.getItem(0) : rawNode.ownerSVGElement.createSVGTransform();
+				var t = tl.numberOfItems ? tl.getItem(0) : shape.ownerSVGElement.createSVGTransform();
 				var m = t.matrix;
 				var newM = m.multiply(m2);
 				t.setMatrix(newM);
 				tl.initialize(t);
 
-				classContainerRectChildren.push(shape); 
+				//remove him from previous drop target, if it exists
+				//bookkeeping, bookkeeping...
+				var p = shape.parentNode;
+				if(p.classContainerRectChildren){
+					removeFromList(shape,p.classContainerRectChildren);
+				} 
+
+				["classContainerRectXConstraint",
+					"classContainerRectYConstraint",
+					"classContainerRectWidthConstraint",
+					"classContainerRectHeightConstraint"].forEach(function(prop){
+
+					var constraint = p[prop];
+
+					if(constraint){
+						//here we are trying to weed out the nodeAttrs in this constraint dest for which 'shape' is the node
+						constraint.dest.forEach(function(nodeAttrExpr){
+							nodeAttrExpr.nodeAttrs = nodeAttrExpr.nodeAttrs.filter(function(nodeAttr){
+								return nodeAttr.node !== shape;
+							}); 
+						});
+
+						//filter out empty nodeAttrs
+						constraint.dest = constraint.dest.filter(function(nodeAttrExpr){
+							return nodeAttrExpr.nodeAttrs.length; 
+						});
+
+						if(!constraint.dest){
+							//remove constraint from graph and delete property on the parent object
+							removeFromList(constraint,constraintGraph);
+							delete p[prop];	
+						}
+					}
+
+				})
+
+				//now go ahead and add him to the new icon
+				icon.classContainerRectChildren.push(shape); 
 
 				//furthermore, move stuff to be children of the group
 				shape.parentNode.removeChild(shape);
 
 				//TODO: we may want a separate group just for these children
-				//TODO: we may also need to do some matrix normalization
 				icon.appendChild(shape);	
 
 				//containment relationship... for all of his targets
 				//minx, miny for all shapes he contains
 				//maxx, maxy for all shapes he contains
 				//debugger;
-				if(!classContainerRectXConstraint){
-					classContainerRectXConstraint =
+				if(!icon.classContainerRectXConstraint){
+					icon.classContainerRectXConstraint =
 						cm.Constraint(
-							cm.NodeAttr(classContainerRect,"x"),
+							cm.NodeAttr(this,"x"),
 							cm.NodeAttrExpr(shape,"x",cm.dec(PACKAGE_LEFT_PADDING)),
 							Math.min
 						);
 
-					classContainerRectYConstraint =
+					icon.classContainerRectYConstraint =
 						cm.Constraint(
-							cm.NodeAttr(classContainerRect,"y"),
+							cm.NodeAttr(this,"y"),
 							cm.NodeAttrExpr(shape,"y",cm.dec(PACKAGE_TOP_PADDING)),
 							Math.min
 						);
 
-					classContainerRectWidthConstraint =
+					icon.classContainerRectWidthConstraint =
 						cm.Constraint(
-							cm.NodeAttr(classContainerRect,"width"),
-							[cm.NodeAttrExpr(classContainerRect,"x"),
+							cm.NodeAttr(this,"width"),
+							[cm.NodeAttrExpr(this,"x"),
 								cm.NodeAttrExpr(shape,["x","width"],cm.sum)],
 							function(classContainerRectX){
 								//TODO: read arbitrary arguments for second parameter
@@ -388,10 +401,10 @@ function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestL
 							}
 						);
 				
-					classContainerRectHeightConstraint = 
+					icon.classContainerRectHeightConstraint = 
 						cm.Constraint(
-							cm.NodeAttr(classContainerRect,"height"),
-							[cm.NodeAttrExpr(classContainerRect,"y"),
+							cm.NodeAttr(this,"height"),
+							[cm.NodeAttrExpr(this,"y"),
 								cm.NodeAttrExpr(shape,["y","height"],cm.sum)],
 							function(classContainerRectY,shapeBottomY){
 								//TODO: read arbitrary arguments for second parameter
@@ -405,21 +418,21 @@ function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestL
 							}
 						);
 						cm.Constraint(
-							cm.NodeAttr(classContainerRect,"height"),
+							cm.NodeAttr(this,"height"),
 							cm.NodeAttrExpr(shape,["y","height"],cm.sum),
 							Math.max
 						);
 
 					//push
-					constraintGraph.push(classContainerRectXConstraint,
-								classContainerRectYConstraint,
-								classContainerRectWidthConstraint,
-								classContainerRectHeightConstraint);
+					constraintGraph.push(icon.classContainerRectXConstraint,
+								icon.classContainerRectYConstraint,
+								icon.classContainerRectWidthConstraint,
+								icon.classContainerRectHeightConstraint);
 				}else{
-					classContainerRectXConstraint.dest.push(cm.NodeAttrExpr(shape,"x"));
-					classContainerRectYConstraint.dest.push(cm.NodeAttrExpr(shape,"y"));
-					classContainerRectWidthConstraint.dest.push(cm.NodeAttrExpr(shape,["x","width"],cm.sum));
-					classContainerRectHeightConstraint.dest.push(cm.NodeAttrExpr(shape,["y","height"],cm.sum));
+					icon.classContainerRectXConstraint.dest.push(cm.NodeAttrExpr(shape,"x"));
+					icon.classContainerRectYConstraint.dest.push(cm.NodeAttrExpr(shape,"y"));
+					icon.classContainerRectWidthConstraint.dest.push(cm.NodeAttrExpr(shape,["x","width"],cm.sum));
+					icon.classContainerRectHeightConstraint.dest.push(cm.NodeAttrExpr(shape,["y","height"],cm.sum));
 				}
 
 				requestLayout();
@@ -431,7 +444,7 @@ function setupConstructors(defaultStatechartInstance,cm,constraintGraph,requestL
 			}
 
 			classContainerRect.hasHierarchicalChild = function(shape){
-				return classContainerRectChildren.indexOf(shape) !== -1; 
+				return icon.classContainerRectChildren.indexOf(shape) !== -1; 
 			}
 
 
